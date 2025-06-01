064aab090377eea56e4ed8943048eeb62ae0dd4b
Edwin <elaboytorres@gmail.com>
2025-05-31 21:26:32 -0500

    Filtering needed todo

--- frontend/ShiftLedgerWeb/src/Pages/MyShifts.jsx
+++ frontend/ShiftLedgerWeb/src/Pages/MyShifts.jsx
@@ -16,6 +16,7 @@ export default function MyShifts() {
     getMyShifts();
     return (
         <div style={{maxWidth: "1200px", margin: "0 auto", width: "100%"}}>
+            <text>Filtering Needed</text>
             <ScrollList shifts={shifts}/>
         </div>
     )
